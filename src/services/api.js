fceec3b70c9638ffefff380299a9f8b44adedc1e
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-03-17 19:20:42 +0200

    end HW 4 10.0

--- src/services/api.js
+++ src/services/api.js
@@ -4,19 +4,15 @@ axios.defaults.baseURL = "https://api.unsplash.com";
 const ACCESS_KEY = "A3nx2FXRqtZH1-4NqfORpOXAK1JQFT9rylzqShlpDlI";
 
 export const getFetchImg = async (page, per_page, query) => {
-  try {
-    const response = await axios.get("/search/photos", {
-      params: {
-        client_id: ACCESS_KEY,
-        page: page,
-        per_page: per_page,
-        query: query,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error("Failed to fetch images from Unsplash API");
-  }
+  const response = await axios.get("/search/photos", {
+    params: {
+      client_id: ACCESS_KEY,
+      page: page,
+      per_page: per_page,
+      query: query,
+    },
+  });
+  return response.data;
 };
 // export const respImg = async () => {
 //   const response = await axios.get("photos", {
